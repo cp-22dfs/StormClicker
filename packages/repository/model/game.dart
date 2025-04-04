57d566341b9fb4f6da59c5da5320129a8d2ab609
cp-24thb <theo.biselx@ceff.ch>
2025-04-04 10:43:22 +0200

    Added values to the game class

--- packages/repository/model/game.dart
+++ packages/repository/model/game.dart
@@ -1,1 +1,14 @@
-class Game {}
+import 'user.dart';
+
+class Game {
+  List<User> users = [];
+
+  Game({List<User>? listUser}){
+    if(listUser == null){
+      users = [new User(), new User()];
+    }
+    else{
+      users = listUser;
+    }
+  }
+}

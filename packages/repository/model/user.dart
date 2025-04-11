ed128e03278d8012f4ebd770340e19a853253ff2
cp-24thb <theo.biselx@ceff.ch>
2025-04-11 10:33:25 +0200

    Added more values and functions to the game class

--- packages/repository/model/user.dart
+++ packages/repository/model/user.dart
@@ -1,3 +1,30 @@
 class User {
-  
+  final int id;
+  final String pseudo ;
+
+  bool ready;
+
+  User({required this.id, required this.pseudo ,required this.ready});
+
+ 
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'pseudo ': pseudo ,
+      'ready': ready,
+    };
+  }
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      pseudo : json['pseudo '],
+      ready: json['ready']
+    );
+  }
+
+  @override
+  String toString() {
+    return 'User(id: \$id, pseudo : \$pseudo , ready \$ready )';
+  }
 }

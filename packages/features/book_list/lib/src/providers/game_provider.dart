eec9710b854748fa6271a80194be8dcdbb13ea93
cp-24thb <theo.biselx@ceff.ch>
2025-05-02 10:39:03 +0200

    Started the provider for the game

--- packages/features/book_list/lib/src/providers/game_provider.dart
+++ packages/features/book_list/lib/src/providers/game_provider.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../repository/model/game.dart';
+import '../../../../../repository/model/user.dart';
 
 class GameProvider extends ChangeNotifier {
-    List<Game> _games = [];
+  final List<Game> _games = [];
   List<Game> get games => _games;
 
-  void Addgame(Game game) {
+  void addgame(Game game) {
     _games.add(game);
     notifyListeners(); 
-}
+  }
+
+  void changeGamePassword(String gameId, String newPassword){
+    _games.firstWhere((game) => game.id == gameId).password = newPassword;
+  }
+
+  void removeGame(String gameId){
+    _games.removeWhere((game) => game.id == gameId);
+  }
+
+  void addUser(String gameId, User user){
+    Game game = _games.firstWhere((game) => game.id == gameId);
+    if(game.canBeJoigned()){
+      game.users.add(user);
+    }
+  }
 }
--- packages/repository/model/game.dart
+++ packages/repository/model/game.dart
@@ -1,6 +1,10 @@
 import 'user.dart';
+import 'package:uuid/uuid.dart';
+
+Uuid uuid = Uuid();
 
 class Game {
+  String id = "";
   List<User> users = [];
   String password = "";
   int gameNumber = 1;
@@ -8,10 +12,7 @@ class Game {
   bool gameStarted = false;
   // string bestPlayer = "";
 
-  Game(List<User> listUser, String passwordWord) {
-    users = listUser;
-    password = passwordWord;
-  }
+  Game({String? id, required this.users, this.password = "12345",}): id = id ?? uuid.v4();
 
   void Restart() {
     gameNumber++;
@@ -21,17 +22,17 @@ class Game {
     gameStarted = false;
   }
 
-  void StartGame(){
-    if(CanStartgame()){
+  void startGame(){
+    if(canStartgame()){
       gameStarted = true;
     }
   }
 
-  bool CanStartgame(){
+  bool canStartgame(){
     return users.length == 2 && users.where((user) => user.ready).length == users.length;
   }
 
-  bool CanBeJoigned(){
+  bool canBeJoigned(){
     return users.length <= 1;
   }
 }

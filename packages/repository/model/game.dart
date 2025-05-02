222380938ac41a3f4219383befd1ecc749e27e42
cp-22tip <Timeo.pellicano@ceff.ch>
2025-05-02 10:07:40 +0200

    Add Game class to main

--- packages/repository/model/game.dart
+++ packages/repository/model/game.dart
@@ -1,18 +1,38 @@
-<<<<<<< HEAD
-class Game {}
-=======
 import 'user.dart';
 
 class Game {
   List<User> users = [];
+  String password = "";
+  int gameNumber = 1;
+  String winner = "";
+  bool gameStarted = false;
+  // string bestPlayer = "";
 
-  Game({List<User>? listUser}){
-    if(listUser == null){
-      users = [new User(), new User()];
+  Game(List<User> listUser, String passwordWord) {
+    users = listUser;
+    password = passwordWord;
+  }
+
+  void Restart() {
+    gameNumber++;
+    for (int i = 0; i < users.length; i++) {
+      users[i].ready = false;
     }
-    else{
-      users = listUser;
+    gameStarted = false;
+  }
+
+  void StartGame() {
+    if (CanStartgame()) {
+      gameStarted = true;
     }
   }
+
+  bool CanStartgame() {
+    return users.length == 2 &&
+        users.where((user) => user.ready).length == users.length;
+  }
+
+  bool CanBeJoigned() {
+    return users.length <= 1;
+  }
 }
->>>>>>> origin/main

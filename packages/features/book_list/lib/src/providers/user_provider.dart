514bdc89b78989063340ec166525d7fe05dac287
cp-22tip <Timeo.pellicano@ceff.ch>
2025-05-16 09:54:32 +0200

    Ajout des fonctions user provider
    delete, ready et joingame

--- packages/features/book_list/lib/src/providers/user_provider.dart
+++ packages/features/book_list/lib/src/providers/user_provider.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
-
+import '../../../../../repository/model/game.dart';
 import '../../../../../repository/model/user.dart';
 
-class UserProvider  with ChangeNotifier {
+class UserProvider with ChangeNotifier {
   List<User> _users = [];
   List<User> get users => _users;
 
   void AddUser(User user) {
     _users.add(user);
-    notifyListeners(); 
-  }}
+    notifyListeners();
+  }
 
- 
+  void deleteUser(String id) {
+    _users.removeWhere((user) => user.id == id);
+    notifyListeners();
+  }
+
+  void ready(String id) {
+    final index = _users.indexWhere((user) => user.id == id);
+    if (index != -1) {
+      final ancienUser = _users[index];
+      _users[index] = User(
+        id: ancienUser.id,
+        pseudo: ancienUser.pseudo,
+        ready: !ancienUser.ready,
+      );
+      notifyListeners();
+    }
+  }
+
+  void joinGame(Game game, String id) {
+    final index = _users.indexWhere((user) => user.id == id);
+  }
+}
